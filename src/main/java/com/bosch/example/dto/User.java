3a7feb29a685c434a2446ea65643632cfdd1d938
JoviOakz <joao.mateussdc@gmail.com>
2024-06-10 16:18:14 -0300

    feat: UserController.java

--- src/main/java/com/bosch/example/dto/User.java
+++
@@ -1,7 +0,0 @@
-package com.bosch.example.dto;
-
-public record User(
-    String name,
-    String email,
-    String password
-) {}
--- src/main/java/com/bosch/example/repositories/UserJPARepository.java
+++ src/main/java/com/bosch/example/repositories/UserJPARepository.java
@@ -3,7 +3,7 @@ package com.bosch.example.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.bosch.example.dto.User;
+import com.bosch.example.model.User;
 
 @Repository
 public interface UserJPARepository extends JpaRepository<User, Long> {}
